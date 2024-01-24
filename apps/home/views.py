6772c95fa588fdf7263e7bfcdacfce8e6a5d9a1e
Leonardo Beroes <leonardoberoes94@gmail.com>
2024-01-23 23:34:44 -0400

    feat: add SubscriptionView and logic to list subscription from sidebar

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,6 +8,12 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+from apps.home.forms import SubscriptionForm
+from apps.home.models import Subscription
 
 
 @login_required(login_url="/login/")
@@ -29,6 +35,25 @@ def pages(request):
 
         if load_template == 'admin':
             return HttpResponseRedirect(reverse('admin:index'))
+        elif load_template == 'ui-list-subscript-users.html':
+            query_set = Subscription.objects.all()
+
+            context['data'] = [{
+                'user': each.user,
+                'credit_card': each.credit_card,
+                'activation_date': each.activation_date,
+                'preferences': each.preferences,
+                'level': each.level
+
+            } for each in query_set]
+
+            context['titles'] = ['user',
+                                 'credit_card',
+                                 'activation_date',
+                                 'preferences',
+                                 'level'
+                                 ]
+
         context['segment'] = load_template
 
         html_template = loader.get_template('home/' + load_template)
@@ -42,3 +67,11 @@ def pages(request):
     except:
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
+
+
+class SubscriptionView(LoginRequiredMixin, CreateView):
+    """Create a new post."""
+
+    template_name = 'home/ui-add-subscription.html'
+    form_class = SubscriptionForm
+    success_url = reverse_lazy('home')
