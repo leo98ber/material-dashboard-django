046c1f3f90da69642907490c71107ad5d8acc68b
Leonardo Beroes <leonardoberoes94@gmail.com>
2024-01-23 23:34:12 -0400

    feat: add SubscriptionView and logic to list subscription from sidebar

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -6,11 +6,13 @@ Copyright (c) 2019 - present AppSeed.us
 from django.urls import path, re_path
 from apps.home import views
 
+
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
-
+    # Add report
+    path('add_subscription/', views.SubscriptionView.as_view(), name='add_subscription'),
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
 
